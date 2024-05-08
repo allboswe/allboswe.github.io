5229e05d70f052a75e9aeb864d53db3ac2dd4d57
Allan Boswell <144160365+allboswe@users.noreply.github.com>
2024-05-08 18:19:38 -0500

    Update main.jsx
    Signed-off-by: Allan Boswell <144160365+allboswe@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import Application from './Application.jsx'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>,
 )
