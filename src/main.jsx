01c94e9ce466385609633e1830cbeb76e3e96df9
Allan Boswell <144160365+allboswe@users.noreply.github.com>
2024-10-13 16:18:26 -0500

    Update main.jsx
    Signed-off-by: Allan Boswell <144160365+allboswe@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import Application from './Application.jsx'
+
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
