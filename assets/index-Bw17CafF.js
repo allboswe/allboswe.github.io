3a3768d87df9c7e59483ab95eb71d35320980c97
Allan Boswell <144160365+allboswe@users.noreply.github.com>
2026-02-11 02:07:32 -0600

    refactor: updated assets/index-Bw17CafF.js
    Signed-off-by: Allan Boswell <144160365+allboswe@users.noreply.github.com>

--- assets/index-Bw17CafF.js
+++ assets/index-Bw17CafF.js
@@ -70,7 +70,7 @@
           </div>
           <div class="card-content">
             <h3 class="card-title">Story Title</h3>
-            <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla feugiat metus, a hendrerit tellus viverra in. Mauris tempus tortor sit amet sagittis ullamcorper. Aenean. </p>
+            <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <ul class="card-tags">
               <li class="tag">adventure</li>
             </ul>
