091114a770560681c463116e1e795d50a149d852
allboswe <144160365+allboswe@users.noreply.github.com>
2026-04-13 01:53:53 -0500

    refactor: updated src/pages/home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,17 +3,17 @@ import { renderStandardPage } from '../components/layout';
 export function renderHomePage() {
   return renderStandardPage(`
     <section id="hero">
-      <h2>Hello.</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
+      <h2>Hello world.</h2>
+      <p>I build software and write stories.</p>
       <div>
         <button type="button">View Projects</button>
-        <button type="button">Read Manga</button>
+        <button type="button">Read Light Novels</button>
       </div>
     </section>
 
     <section id="projects">
       <h2>Projects</h2>
-      <p class="section-intro">Lorem ipsum dolor sit amet.</p>
+      <p class="section-intro">Stuff I've made.</p>
       <div class="card-list">
         <article class="card">
           <div class="card-image">
@@ -33,9 +33,9 @@ export function renderHomePage() {
       </div>
     </section>
 
-    <section id="manga">
-      <h2>Manga</h2>
-      <p class="section-intro">Lorem ipsum dolor sit amet.</p>
+    <section id="light-novels">
+      <h2>Light Novels</h2>
+      <p class="section-intro">Stuff I write when I should be doing something else.</p>
       <div class="card-list">
         <article class="card">
           <div class="card-image">
