c21290037caaf6e7bd76e10a4958cb51ae2d12f6
Allan Boswell <144160365+allboswe@users.noreply.github.com>
2026-04-25 18:19:14 -0500

    feature: updated assets/index-CZ1664Fi.js
    Signed-off-by: Allan Boswell <144160365+allboswe@users.noreply.github.com>

--- assets/index-CZ1664Fi.js
+++ assets/index-CZ1664Fi.js
@@ -200,6 +200,21 @@
 
             <dl class="character-facts">
               <div class="character-facts-section">
+                <dt>Name</dt>
+              </div>
+
+              <div>
+                <dt>Kana</dt>
+                <dd>ロンドン</dd>
+              </div>
+              <div>
+                <dt>Romaji</dt>
+                <dd>Rondon</dd>
+              </div>
+            </dl>
+
+            <dl class="character-facts">
+              <div class="character-facts-section">
                 <dt>First Appearance</dt>
               </div>
 
