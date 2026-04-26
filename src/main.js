8096e763c3a534c2080dcf6f4358ea2de4fa5d55
allboswe <144160365+allboswe@users.noreply.github.com>
2026-04-26 05:56:20 -0500

    feature: updated src/main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './styles/index.css';
+import { initSpoilerToggle } from './libraries/spoilers';
 import { initThemeToggle } from './libraries/theme';
 import { renderHomePage } from './pages/home';
 import { renderLondonPage } from './pages/london';
@@ -20,6 +21,7 @@ if (path === '/pink_guy/characters/london') {
 }
 
 initThemeToggle();
+initSpoilerToggle();
 
 /* Menu Logic */
 
