2384be48a724db97d83b91231ad3e4ead4dd6407
Allan Boswell <144160365+allboswe@users.noreply.github.com>
2026-06-28 20:39:23 -0500

    [Chore] Remove Wiki (#9)
    Signed-off-by: Allan Boswell <144160365+allboswe@users.noreply.github.com>

--- src/components/character-overview.js
+++
@@ -1,7 +0,0 @@
-export function renderCharacterOverview(character) {
-  return `
-    <section class="character-section" id="overview">
-      <p>${character.overview}</p>
-    </section>
-  `;
-}
--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
 import './styles/index.css';
-import { initGalleryLightbox } from './libraries/gallery-lightbox';
-import { initSpoilerToggle } from './libraries/spoilers';
 import { initThemeToggle } from './libraries/theme';
-import { initCharacterToc } from './libraries/toc';
-import { initHeadingToggle } from './libraries/heading-toggle';
 import { renderHomePage } from './pages/home';
-import { renderDandyPage } from './pages/dandy';
-import { renderDandyGalleryPage } from './pages/dandy-gallery';
-import { renderLondonPage } from './pages/london';
-import { renderLondonGalleryPage } from './pages/london-gallery';
 
 const app = document.querySelector('#app');
 
 const pages = {
   home: renderHomePage,
-  dandy: renderDandyPage,
-  london: renderLondonPage,
-  'dandy-gallery': renderDandyGalleryPage,
-  'london-gallery': renderLondonGalleryPage,
 };
 
 const pageName = app?.dataset.page || 'home';
@@ -28,10 +16,6 @@ if (app) {
 }
 
 initThemeToggle();
-initCharacterToc();
-initSpoilerToggle();
-initHeadingToggle();
-initGalleryLightbox();
 
 /* Menu Logic */
 
